83fdc699cedd4981f2d2e098217e0f78b71cb52a
bishop <bishopbishal256@gmail.com>
2024-01-11 12:52:42 +0530

    added the code example for linear search and have done 2 leetcode questions

--- src/main/java/org/datastructure/Algorithms/LinearSearch.java
+++ src/main/java/org/datastructure/Algorithms/LinearSearch.java
@@ -26,12 +26,77 @@ public class LinearSearch {
         System.out.println("Minimum of the number is  "+minimum);
         int maximumIn2DArray = findMaximumIn2DArray(array2D);
         System.out.println("Maximum of number in 2DArray is   "+maximumIn2DArray);
+
+
+        /* 
+         * LeetCode Questions
+         * 
+         * 1)Given an array nums of integers, return how many of them contain an even number of digits.
+         * Input: nums = [12,345,2,6,7896]
+            Output: 2
+         * 
+         * You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+         * Input: accounts = [[1,2,3],[3,2,1]]
+         *   Output: 6
+         * 
+         */
+
+         int[] nums = {12,345,2,6,7896};
+        int nuumberOfEvenDigit=countEvenNumberOfDigit(nums);
+        System.out.println("Number of Even digits are = "+nuumberOfEvenDigit);
+
+        int[][] accounts={{1,2,3},{3,2,1}};
+        int[][] accounts1={{1,5},{7,3},{3,5}};
+        int[][] accounts2={{2,8,7},{7,1,3},{1,9,5}};
+
+         System.out.println("Maximum Wealth is = "+findWealth(accounts));    
+         System.out.println("Maximum Wealth is = "+findWealth(accounts1));    
+         System.out.println("Maximum Wealth is = "+findWealth(accounts2));    
+         
      }
 
 
      
 
-     private static int findMaximumIn2DArray(int[][] array2d) {
+    private static int findWealth(int[][] accounts) {
+        int max=0;
+      for (int[] is : accounts) {
+        int sum=0;
+        for (int is2 : is) {
+          sum+=is2;
+        }
+        if(max<sum)
+        {
+            max=sum;
+        }
+      }
+    return max;
+    }
+
+
+
+
+    private static int countEvenNumberOfDigit(int[] nums) {
+         int max=0;
+        for (int i : nums) {
+            int count=0;
+            while(i>0)
+            {
+                i=i/10;
+                count++;
+            }
+            if(count%2==0)
+              {
+               max++;
+              }
+        }
+        return max;
+    }
+
+
+
+
+    private static int findMaximumIn2DArray(int[][] array2d) {
         int max=array2d[0][0];
         for (int[] is : array2d) {
             for (int is2 : is) {
